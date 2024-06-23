5cd012bcbfba44fb4f2009bbf94c9c1b7c8ce361
reza drakhshan <github2reza@gmail.com>
2024-06-23 15:48:30 +0330

    complete Hamburger Menu maybe

--- Hamburger Menu/js/app.js
+++ Hamburger Menu/js/app.js
@@ -1,12 +1,13 @@
-let btn_open = document.getElementsByClassName("btn-open")[0]
-let sidebar_close = document.getElementsByClassName("sidebar-close")[0]
-let sidebar = document.getElementsByClassName("sidebar")[0]
+let btn_open = document.getElementsByClassName("btn-open")[0];
+let sidebar_close = document.getElementsByClassName("sidebar-close")[0];
+let sidebar = document.getElementsByClassName("sidebar")[0];
 
-btn_open.addEventListener("click", () =>{
-    sidebar.classList.toggle("hide")
-    btn_open.classList.toggle("hide-btn")
-})
-sidebar_close.addEventListener("click",() => {
-    sidebar.classList.toggle("hide")
-    btn_open.classList.toggle("hide-btn")
-})
+btn_open.addEventListener("click", () => {
+    sidebar.classList.add("show");
+    btn_open.classList.add("hide-btn");
+});
+
+sidebar_close.addEventListener("click", () => {
+    sidebar.classList.remove("show");
+    btn_open.classList.remove("hide-btn");
+});
