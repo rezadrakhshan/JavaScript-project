f5c0ac312a0591cde3f1f99707513ef0b5809ecb
reza drakhshan <github2reza@gmail.com>
2024-07-04 15:30:34 +0330

    feat
    I completed the navbar and added the sidebar section

--- Dashboard/js/book.js
+++ Dashboard/js/book.js
@@ -1,11 +1,22 @@
 const sidebarBtn = document.getElementsByTagName("button")[0]
-const sidebarCloseBtn = document.getElementsByTagName("button")[1]
 const sidebar = document.getElementsByClassName("sidebar")[0]
-const body = document.getElementsByTagName("body")[0]
+const searchBox = document.getElementsByClassName("search-box")[0]
 
-sidebarBtn.addEventListener("click", () => {
+
+sidebarBtn.addEventListener("click", (e) => {
+    e.stopPropagation()
     sidebar.classList.toggle("show")
+    searchBox.classList.remove("open")
 })
-sidebarCloseBtn.addEventListener("click", () => {
-    sidebar.classList.toggle("show")
+document.addEventListener("click", (e) => {
+    if (!sidebar.contains(e.target) && e.target !== sidebarBtn && !searchBox.contains(e.target)) {
+        sidebar.classList.remove("show");
+        searchBox.classList.remove("open")
+    }
+})
+
+document.getElementsByTagName("input")[0].addEventListener("click", (e) => {
+    e.stopPropagation()
+    sidebar.classList.remove("show")
+    searchBox.classList.toggle("open")
 })
