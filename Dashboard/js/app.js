03618e0002bc419b889cc5bc2b38403077a05fde
reza drakhshan <github2reza@gmail.com>
2024-07-04 20:07:51 +0330

    feat
    update is done

--- Dashboard/js/app.js
+++ Dashboard/js/app.js
@@ -1,22 +1,12 @@
-const sidebarBtn = document.getElementsByTagName("button")[0]
-const sidebar = document.getElementsByClassName("sidebar")[0]
 const searchBox = document.getElementsByClassName("search-box")[0]
 
-
-sidebarBtn.addEventListener("click", (e) => {
-    e.stopPropagation()
-    sidebar.classList.toggle("show")
-    searchBox.classList.remove("open")
-})
 document.addEventListener("click", (e) => {
-    if (!sidebar.contains(e.target) && e.target !== sidebarBtn && !searchBox.contains(e.target)) {
-        sidebar.classList.remove("show");
+    if (!searchBox.contains(e.target)) {
         searchBox.classList.remove("open")
     }
 })
 
 document.getElementsByTagName("input")[0].addEventListener("click", (e) => {
     e.stopPropagation()
-    sidebar.classList.remove("show")
     searchBox.classList.toggle("open")
 })
