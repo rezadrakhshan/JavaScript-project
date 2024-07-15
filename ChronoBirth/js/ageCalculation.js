4c23485949c2428fcee9f26484516380dc776c75
reza drakhshan <github2reza@gmail.com>
2024-07-15 17:42:13 +0330

    fix
    Fixed issues with entering the wrong date

--- ChronoBirth/js/ageCalculation.js
+++ ChronoBirth/js/ageCalculation.js
@@ -4,6 +4,13 @@ ageForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const userAge = new Date(e.target.date.value);
   const date = new Date();
-  alert(`You have ${date.getFullYear() - userAge.getFullYear()} years old`);
-  ageForm.reset()
+  const result = date.getFullYear() - userAge.getFullYear()
+  if (result <= 0) {
+    alert("Please enter the date correctly")
+    ageForm.reset()
+  }
+  else{
+    alert(`You have ${result} years old`);
+    ageForm.reset()
+  }
 });
