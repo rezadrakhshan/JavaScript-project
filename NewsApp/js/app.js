0482a2630e18eec1b589b958edfbc197ceb6383f
reza drakhshan <github2reza@gmail.com>
2024-08-23 23:08:34 +0330

    feat
    Add NewsApp : searchbox completed,show data completed

---
+++ NewsApp/js/app.js
@@ -0,0 +1,63 @@
+const container = document.querySelector(".container");
+const form = document.querySelector("form");
+
+class Main {
+  constructor(container, form) {
+    this.container = container;
+    this.form = form;
+    this.url =
+      "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=c71f8e2dbd594a86a10f7f073ce19aa2";
+    this.addEventListener();
+    this.getData();
+  }
+  addEventListener() {
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.searchNews(e);
+    });
+  }
+  async getData() {
+    try {
+      const response = await axios.get(this.url);
+      const data = await response.data.articles;
+      this.showData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+  showData(list) {
+    this.container.innerHTML = ""; 
+    list.forEach((element) => {
+      const newCard = document.createElement("a");
+      newCard.href = element.url;
+      const newdiv = document.createElement("div");
+      newdiv.classList.add("card");
+      newCard.appendChild(newdiv);
+      newdiv.innerHTML = `
+            <img src="${element.urlToImage}" alt="600x400">
+            <h1>${element.title}</h1>
+            <p>${element.description}</p>
+            `;
+      this.container.appendChild(newCard);
+    });
+  }
+  async searchNews(e) {
+    const UserSelectedWord = e.target.key.value;
+    try {
+      const response = await axios.get(this.url);
+      const data = await response.data.articles;
+      const userSearchResult = data.filter((item) => {
+        return item.title
+          .toLowerCase()
+          .includes(UserSelectedWord.toLowerCase());
+      });
+      this.showData(userSearchResult);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  new Main(container, form);
+});
